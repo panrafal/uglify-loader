5b3e86661c44f758899f72ce5597361fc8026c5b
Rafal Lindemann <rl@stamina.pl>
2016-03-22 10:20:32 +0100

    Compose with original sourcemap

--- index.js
+++ index.js
@@ -11,17 +11,25 @@ module.exports = function(source, inputSourceMap) {
     var opts = this.options['uglify-loader'] || {};
     // just an indicator to generate source maps, the output result.map will be modified anyway
     // tell UglifyJS2 not to emit a name by just setting outSourceMap to true
-    opts.outSourceMap = true;
+    opts.outSourceMap = !!(this.sourceMap || inputSourceMap);
+    opts.inSourceMap = inputSourceMap
     opts.fromString = true;
 
     var result = UglifyJS.minify(source, opts);
 
-    var sourceFilename = loaderUtils.getRemainingRequest(this);
-    var current = loaderUtils.getCurrentRequest(this);
-    var sourceMap = JSON.parse(result.map);
-    sourceMap.sources = [sourceFilename];
-    sourceMap.file = current;
-    sourceMap.sourcesContent = [source];
-
-    callback(null, result.code, sourceMap);
+    if (result.map) {
+        var sourceFilename = loaderUtils.getRemainingRequest(this);
+        var current = loaderUtils.getCurrentRequest(this);
+        var sourceMap = JSON.parse(result.map);
+        sourceMap.sources = [sourceFilename];
+        sourceMap.file = current;
+        if (inputSourceMap) {
+            sourceMap.sourcesContent = inputSourceMap.sourcesContent;
+        } else {
+            sourceMap.sourcesContent = [source];
+        }
+        callback(null, result.code, sourceMap);
+    } else {
+        callback(null, result.code);
+    }
 };
